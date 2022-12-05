dc155629c4bcb8eae87407967d4cc777a3b9e438
Martin Pfundmair <dev@martin.pfundmair.de>
2022-12-06 06:52:40 +0900

    Update availability checks (#13)
    for navigationTitle

--- Sources/AckGenUI/CustomNavigationTitle.swift
+++ Sources/AckGenUI/CustomNavigationTitle.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-@available(iOS 14, *)
+@available(iOS 14, watchOS 7.0, macOS 11, *)
 struct NavigationTitleViewModifier: ViewModifier {
     var text: String
 
@@ -20,14 +20,17 @@ struct NavigationBarTitleViewModifier: ViewModifier {
     var text: String
 
     func body(content: Content) -> some View {
-        content.navigationBarTitle(text)
+        content
+        #if os(iOS) || os(watchOS)
+            .navigationBarTitle(text)
+        #endif
     }
 }
 
 extension View {
     @ViewBuilder
     func customNavigationTitle(_ text: String) -> some View {
-        if #available(iOS 14, *) {
+        if #available(iOS 14, watchOS 7.0, macOS 11, *) {
             self.modifier(NavigationTitleViewModifier(text: text))
         }
         else {
